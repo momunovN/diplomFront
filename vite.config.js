d3e712274ba994ffa96074051e868fb1f0d1fc61
Your Name <your-email@example.com>
2026-01-07 18:43:32 +0300

    Нащел

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://diplomback-1.onrender.com',
         changeOrigin: true,
       }
     }
